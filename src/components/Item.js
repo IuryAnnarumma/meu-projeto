95557ddc8e1227e3f5d8d615a977e250772ca21a
Iury Annarumma <106461107+IuryAnnarumma@users.noreply.github.com>
2022-08-26 18:02:10 -0300

    fragments

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,23 @@
-function Item( marca, ano_lancamento ) {
+import PropTypes from 'prop-types';
+
+function Item( {marca, ano_lancamento}) {
     return (
         <>
-          <li> {marca} - {ano_lancamento} </li>
+          <li> 
+            {marca} - {ano_lancamento} 
+          </li>
         </>
     )
+}
+
+Item.propTypes = {
+    marca: PropTypes.string.isRequired,
+    ano_lancamento: PropTypes.number.isRequired,
+}
+
+Item.defaultProps = {
+    marca: 'Faltou a marca',
+    ano_lancamento: 0,
 }
 
 export default Item
